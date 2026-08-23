fcb0430a6ee1457c0d39093057a6e29503c04beb
mhdieleven <72516717+mhdiEleven@users.noreply.github.com>
2026-08-23 17:42:43 +0100

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights: number[]): number {
+        let l = 0
+        let r = heights.length -1
+        let bestResult = -Infinity
+
+        while(l<=r){
+            let width = r-l
+            let height = Math.min(heights[l],heights[r])
+            bestResult =Math.max(bestResult, height*width)
+            if(heights[l] == height) l++
+            if(heights[r] == height) r--
+        }
+        return bestResult
+    }
+}
