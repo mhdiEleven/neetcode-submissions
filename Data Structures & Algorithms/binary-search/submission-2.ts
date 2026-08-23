40cc29ae2735c090dba1e3bb648795cd99683891
mhdieleven <72516717+mhdiEleven@users.noreply.github.com>
2026-08-23 23:23:05 +0100

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.ts
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums: number[], target: number): number {
+        let l = 0 
+        let r = nums.length -1
+        while(l<=r){
+            let m = Math.floor((l+r)/2)
+            if(nums[m]<target){l = m+1}
+            else if(nums[m]>target) r=m-1
+            else if(nums[m] == target) return m
+        }
+        return -1
+
+    }
+}
