1dc3f66a33e9a763c627197bcff4fa11a960c2a3
mhdieleven <72516717+mhdiEleven@users.noreply.github.com>
2026-08-24 13:14:28 +0100

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.ts
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s: string): boolean {
+        const open = new Set(['(','[','{'])
+        const close = new Set([')',']','}'])
+        const openIndex = new Map([
+            ['(',')'],
+            ['[',']'],
+            ['{','}']
+        ])
+        let stack = []
+        for(let i =0 ; i<s.length ; i++){
+            if (open.has(s[i])) stack.push(s[i])
+            if(close.has(s[i])){
+                let value = stack.pop()
+                if (openIndex.get(value) == s[i]) continue
+                return false
+
+            }
+            
+        }
+        if(stack.length !== 0) return false
+        return true
+    }
+}
